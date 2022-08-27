bf65d732bcb216c08927fb28c645890199e83e72
nabawi16 <a.nabawi16@gmail.com>
2022-08-27 17:45:34 +0700

    push1

---
+++ Test Suites/Test Cases.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Cases</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>5daaa487-45e8-4d38-85ae-c9cdcde2a444</testSuiteGuid>
+   <testCaseLink>
+      <guid>64c21600-f7ae-41b6-82eb-6b79b3d82a49</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/GET list users</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d89bfda3-828f-4675-861a-b5a479076075</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/GET single user</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8841bed7-fd07-4331-b2d8-1a53f885aaaf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/POST register successful</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4a5dadfa-7aac-4816-a94a-82c630da4289</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/PUT update</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
